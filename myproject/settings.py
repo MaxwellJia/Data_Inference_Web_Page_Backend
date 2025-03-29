bdcabd91d757956752cf4b7ca952ec18b422f8bc
MaxwellJia <591790421@qq.com>
2025-03-29 23:22:07 +1100

    change settings to deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
 
 # STATIC_URL configuration, used for serving staticfiles files
 STATIC_URL = './static'  # The URL path for staticfiles files (CSS, JS, images)
-STATIC_ROOT = os.path.join('./../staticfiles', 'staticfiles')
+STATIC_ROOT = os.path.join('./../myproject/staticfiles', 'staticfiles')
 
 # Optional: If you need to load staticfiles files in development, you can specify directories here
 # if DEBUG:
@@ -77,7 +77,7 @@ ALLOWED_HOSTS = ['*']  # Allow all hosts in development, but should be restricte
 DEBUG = false  # Enable debug mode for more detailed error messages during development
 
 # ROOT_URLCONF configuration to specify the root URL configuration module
-ROOT_URLCONF = 'myproject.urls'  # Defines the URL routing for the project
+ROOT_URLCONF = 'urls'  # Defines the URL routing for the project
 
 # SECRET_KEY is used for cryptographic operations such as signing cookies, tokens, etc. (keep it secure)
 SECRET_KEY = '147258j'  # Change this to a secure, random key in production
