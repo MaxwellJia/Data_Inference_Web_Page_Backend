7ae9047b6294caa857ac84c3eb53e1c08492ddbd
MaxwellJia <591790421@qq.com>
2025-03-30 15:32:57 +1100

    set sh to help azure recognize

--- startup.sh
+++ startup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+# 切换到应用目录
+cd /home/site/wwwroot
+
 # 安装依赖
 pip install -r requirements.txt
 
-# 切换到 backend 目录
-cd myproject
-
-# 激活虚拟环境 (如果你使用了虚拟环境)
-# source venv/bin/activate   # 如果你有虚拟环境，取消注释这一行
-
-# 运行 Django 项目，使用 gunicorn
-gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
+# 启动Gunicorn
+gunicorn --bind=0.0.0.0:8000 myproject.myproject.wsgi --chdir /home/site/wwwroot
