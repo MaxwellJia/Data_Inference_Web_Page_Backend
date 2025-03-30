11ff82903cea0e2114a89ce64edeff56457f65f8
MaxwellJia <591790421@qq.com>
2025-03-30 15:23:16 +1100

    set sh to help azure recognize

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 切换到 backend 目录
-cd myproject
-
 # 安装依赖
 pip install -r requirements.txt
+
+# 切换到 backend 目录
+cd myproject
 
 # 激活虚拟环境 (如果你使用了虚拟环境)
 # source venv/bin/activate   # 如果你有虚拟环境，取消注释这一行
