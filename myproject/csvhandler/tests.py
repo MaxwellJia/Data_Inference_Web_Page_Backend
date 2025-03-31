10a21879ddf24d51132419b40e63120e6b6c15c6
MaxwellJia <591790421@qq.com>
2025-03-31 16:59:29 +1100

    try to fix no csvhandler problem

--- myproject/csvhandler/tests.py
+++ myproject/csvhandler/tests.py
@@ -1,20 +1,20 @@
-import os
-import django
-from django.db.models import QuerySet
-
-# Set the Django environment variables
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')  # replace to settings path
-django.setup()  # initialize Django evn
-
-
-from django.test import TestCase
-from csvhandler.models import Student
-
-# Create your tests here.
-if __name__ == '__main__':
-    all_samples: QuerySet[Student] = Student.objects.all()
-    samples = Student.objects.filter(gender=1)
-    for student in all_samples:
-        print(student.name)
-
-    print(all_samples)
+# import os
+# import django
+# from django.db.models import QuerySet
+#
+# # Set the Django environment variables
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')  # replace to settings path
+# django.setup()  # initialize Django evn
+#
+#
+# from django.test import TestCase
+# from csvhandler.models import Student
+#
+# # Create your tests here.
+# if __name__ == '__main__':
+#     all_samples: QuerySet[Student] = Student.objects.all()
+#     samples = Student.objects.filter(gender=1)
+#     for student in all_samples:
+#         print(student.name)
+#
+#     print(all_samples)
